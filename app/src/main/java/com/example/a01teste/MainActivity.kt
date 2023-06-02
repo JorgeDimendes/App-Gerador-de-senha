366553e26abfb1a6f39ffbdb11e273cb21734983
Jorge <jorgedimendes@hotmail.com>
2023-06-02 04:38:49 -0300

    Iniciando Repositorio

---
+++ app/src/main/java/com/example/a01teste/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.a01teste
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+private lateinit var binding: ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
